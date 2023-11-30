33a11b09829b1497e41dd024366845e0a9db13af
kemuma200 <naila.monyonko@gmail.com>
2023-11-29 22:10:59 -0800

    Remove at position

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,7 +2,7 @@
 def magic_calculation(a, b, c):
     """function that does exactly the same as the following Python bytecode"""
     if a < b:
-        return c;
+        return c
     if c > b:
         return (a + b)
     return (a*b - c)
