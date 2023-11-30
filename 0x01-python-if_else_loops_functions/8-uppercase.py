5dd7e87b622d7e70532e2b24c4bd8f060bbb0878
kemuma200 <naila.monyonko@gmail.com>
2023-11-29 17:58:17 -0800

    Prints string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    """Prints a string in uppercase form"""
+    for c in str:
+        if ord(c) >= 97 and ord(C) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
