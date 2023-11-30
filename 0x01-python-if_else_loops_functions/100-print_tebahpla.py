1fcaaf8c0539498ba3280e685c91236bef79d0ca
kemuma200 <naila.monyonko@gmail.com>
2023-11-29 21:07:32 -0800

    Prints alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 f = 0
-for i in range(122, 96, -1):
-    printf("{}".format(chr(c - f)), end="")
-    f == 0 ? f = 32 : f = 0
+for c in range(122, 96, -1):
+    print("{}".format(chr(c - f)), end="")
+    if f == 0:
+        f = 32
+    else:
+        f = 0
