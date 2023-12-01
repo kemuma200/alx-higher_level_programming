187f54c18cca653f076a61d58130e66289467936
kemuma200 <naila.monyonko@gmail.com>
2023-11-30 18:38:42 -0800

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 != 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
     operator = {"+":add, "-":sub, "*":mul, "/":div}
     if sys.argv[2] not in list(operator.keys()):
