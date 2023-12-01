1e9b10ea3c20f608a2d1cbbc30730899454c6510
kemuma200 <naila.monyonko@gmail.com>
2023-11-30 18:05:26 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    if i in names:
-        if i != "__":
+    for i in names:
+        if i[:2] != "__":
             print(i)
