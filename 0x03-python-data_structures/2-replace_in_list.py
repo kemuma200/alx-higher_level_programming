0b64688d8e640b277bc0e12d33aad1c0f9c16177
kemuma200 <naila.monyonko@gmail.com>
2023-12-04 01:48:19 -0800

    Replace or with and

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     """ function that replaces an element of a list at a specific position"""
-    if idx >= 0 or idx < len(my_list):
+    if idx >= 0 and idx < len(my_list):
     	my_list[idx] = element
     return my_list
     
