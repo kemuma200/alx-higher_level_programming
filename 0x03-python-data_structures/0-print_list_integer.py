48d12f519f7e93762bb20e96038b172dabd5647f
kemuma200 <naila.monyonko@gmail.com>
2023-12-04 01:24:43 -0800

    Removed line which checks if item is an integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,4 @@
 def print_list_integer(my_list=[]):
     """function that prints all integers of a list"""
     for i in range(len(my_list)):
-        if int(my_list[i]) == i:
-            print("{:d}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
