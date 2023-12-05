bda12e05eb5ae712e5924ad62609b38e6ed46639
kemuma200 <naila.monyonko@gmail.com>
2023-12-04 18:56:41 -0800

    Divisible by two

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
+    """function that finds the biggest integer of a list"""
+    
     if len(my_list) == 0:
         return None
+    
     _max = 0
     for x in range(len(my_list)):
         if my_list[x] > _max:
