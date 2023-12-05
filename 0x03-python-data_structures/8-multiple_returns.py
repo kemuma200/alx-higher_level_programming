80a561cef2659d998687ccd3d25cfa4b094154d5
kemuma200 <naila.monyonko@gmail.com>
2023-12-04 18:49:42 -0800

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def multiple_returns(sentence):
     """ function that returns a tuple with the length of a string and its first character"""
     if len(sentence) == 0:
-        return ("{}, {}".format(0, None)
-    return ("{}, {}".format(len(sentence), sentence[0]))
+        return (0, None)
+    return (len(sentence), sentence[0])
