30d958748273c4e8c180e9054e27eb8cef8ff098
kemuma200 <naila.monyonko@gmail.com>
2023-12-08 22:19:45 -0800

    Some additions

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_dict = {
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+    }
+    if !isinstance(roman_string, string) or roman_string is None:
+        return (0);
+    
