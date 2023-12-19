93962776d40c737871b6267336fba4432df897f7
kemuma200 <naila.monyonko@gmail.com>
2023-12-18 18:42:49 -0800

    Trying to tackle test 6

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,9 @@
 
 def safe_print_list(my_list=[], x=0):
     """function that prints x elements of a list"""
+
     _length = 0
+    
     for i in range(x):
         try:
             print(my_list[i], end="")
