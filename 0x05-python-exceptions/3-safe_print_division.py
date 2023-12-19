a3e0cb321971da1bd4b7e0af47c1da9b730fea69
kemuma200 <naila.monyonko@gmail.com>
2023-12-18 18:17:55 -0800

    Safe print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """ function that divides 2 integers and prints the result."""
+    try:
+        res = a / b
+    except (ZeroDivisionError, TypeError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return res
