407c9ed12c3bedd6b0b090cf4a71501cb5c1a5da
kemuma200 <naila.monyonko@gmail.com>
2024-01-04 14:56:59 -0800

    Simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-class Rectangle:
-    pass
+class Rectangle():
+    """Represent a rectangle"""
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
