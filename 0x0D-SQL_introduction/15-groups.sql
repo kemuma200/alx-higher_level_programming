0ecce1f1b0dbf3625c8b68866a20161e39cdca2b
monyonko <monyonko.miriam@gmail.com>
2024-09-26 06:39:33 -0700

    group by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- script that lists the number of records with the same score in the table second_table
-SELECT score, COUNT(1) as numb FROM second_table
-GROUP BY score ORDER BY numb DESC;
+SELECT score, COUNT(1) as number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
