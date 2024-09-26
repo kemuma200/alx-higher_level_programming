3b052d12fbb8fa0e1b0c50f3ead89c7f7352521a
monyonko <monyonko.miriam@gmail.com>
2024-09-26 06:00:47 -0700

    Create and populate

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,6 @@
 -- Script that creates a table
 CREATE table IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
 -- Add values
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1,'John',10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2,'Alex',= 3);
