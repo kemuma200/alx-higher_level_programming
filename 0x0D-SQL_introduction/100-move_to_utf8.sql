5a56cc74645f1be2e652979b57b18b565d973bcb
monyonko <monyonko.miriam@gmail.com>
2024-09-26 07:18:13 -0700

    utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++
@@ -1,6 +0,0 @@
--- script that converts hbtn_0c_0 database to UTF8
--- alter database
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- alter table
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- alter field
