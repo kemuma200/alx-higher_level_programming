a03e153f734ec1497c883b3bf807e4619bd4efb6
monyonko <monyonko.miriam@gmail.com>
2024-09-26 05:57:07 -0700

    Create and populate

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,7 @@
 -- Script that creates a table
 CREATE table IF NOT EXISTS second_table (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`)
-VALUES
-	(1,'John',10),
-	(2,'Alex',= 3),
-	(3,'Bob',14),
-	(4,'George',8);
+-- Add values
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1,'John',10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2,'Alex',= 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3,'Bob',14);
+INSERT INTO `swcond_table` (`id`, `name`, `score`) VALUES (4,'George',8);
