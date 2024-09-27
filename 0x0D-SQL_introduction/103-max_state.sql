5de042c6f0d82e6c7285e21226915c6cb212effd
monyonko <monyonko.miriam@gmail.com>
2024-09-27 14:18:01 -0700

    Max

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Max temp of each state
 
-SELECT `state`, MAX(`value`) AS max_temp FROM `temperatures` ORDER BY `state`;
+SELECT `state`, MAX(`value`) AS max_temp FROM `temperatures` GROUP BY `state` ORDER BY `state`;
